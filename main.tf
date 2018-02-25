8b186ef096a41e2cff8a7a81838a626fbf9aac88
Frank Cash <cashc@acm.org>
2018-02-24 22:29:08 -0500

    Basic TF set-up

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "demo_lambda" {
+    function_name = "demo_lambda"
+    handler = "main"
+    runtime = "go1.x"
+    filename = "./function/function.zip"
+    source_code_hash = "${base64sha256(file("./function/function.zip"))}"
+    role = "${aws_iam_role.lambda_exec_role.arn}"
+}
+
