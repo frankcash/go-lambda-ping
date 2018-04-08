d0bbeeab9528db06854d52fdecff728c8b155154
Frank Cash <cashc@acm.org>
2018-04-08 10:00:05 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -22,10 +23,10 @@ EOF
 }
 
 resource "aws_lambda_function" "demo_lambda" {
-    function_name = "demo_lambda"
-    handler = "main"
-    runtime = "go1.x"
-    filename = "./function/function.zip"
-    source_code_hash = "${base64sha256(file("./function/function.zip"))}"
-    role = "${aws_iam_role.lambda_exec_role.arn}"
+  function_name    = "demo_lambda"
+  handler          = "main"
+  runtime          = "go1.x"
+  filename         = "./function/function.zip"
+  source_code_hash = "${base64sha256(file("./function/function.zip"))}"
+  role             = "${aws_iam_role.lambda_exec_role.arn}"
 }
